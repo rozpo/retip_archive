11af9a222a3009ccfb0d0932d099d2ac313c22ac
Piotr Rozpończyk <rozpo.dev@gmail.com>
2024-08-02 22:07:01 +0100

    Prototype basic layout

--- lib/core/layout/retip_layout.dart
+++ lib/core/layout/retip_layout.dart
@@ -12,12 +12,51 @@ class RetipLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: const Drawer(),
-      appBar: AppBar(
-        title: Text(title),
+    final tabs = [
+      'Genres',
+      'Artists',
+      'Albums',
+      'Songs',
+    ];
+
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+          bottom: TabBar(
+            isScrollable: false,
+            tabs: tabs.map((tab) => Tab(text: tab)).toList(),
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.grid_view_outlined),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: body,
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 0,
+          items: const [
+            BottomNavigationBarItem(
+              activeIcon: Icon(Icons.library_music),
+              icon: Icon(Icons.library_music_outlined),
+              label: 'Library',
+            ),
+            BottomNavigationBarItem(
+              activeIcon: Icon(Icons.headphones),
+              icon: Icon(Icons.headphones_outlined),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              activeIcon: Icon(Icons.settings),
+              icon: Icon(Icons.settings_outlined),
+              label: 'Settings',
+            ),
+          ],
+        ),
       ),
-      body: body,
     );
   }
 }
