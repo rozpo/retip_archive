e24408f529fed5606c572bd27333adc9dd6fe875
Piotr Rozpończyk <rozpo.dev@gmail.com>
2024-08-03 09:30:43 +0100

    Add library categories placeholder

---
+++ lib/app/views/albums/albums_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AlbumsView extends StatelessWidget {
+  const AlbumsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/app/views/songs/songs_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SongsView extends StatelessWidget {
+  const SongsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
