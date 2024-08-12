0cb3f0d9adc696239b1df8b46a556e497a2413c9
Piotr Rozpończyk <rozpo.dev@gmail.com>
2024-08-12 07:28:16 +0100

    Revert to default navigation

--- lib/app/retip_app.dart
+++ lib/app/retip_app.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:retip/app/pages/home/home_page.dart';
 import 'package:retip/app/pages/intro/intro_page.dart';
-import 'package:retip/app/pages/settings/settings_page.dart';
 import 'package:retip/core/l10n/retip_l10n.dart';
 import 'package:retip/core/theme/retip_theme.dart';
-
-enum RetipRoute {
-  home,
-  settings,
-}
 
 class RetipApp extends StatelessWidget {
   const RetipApp({super.key});
@@ -21,23 +14,7 @@ class RetipApp extends StatelessWidget {
       darkTheme: RetipTheme.dark,
       localizationsDelegates: RetipL10n.localizationsDelegates,
       supportedLocales: RetipL10n.supportedLocales,
-      initialRoute: 'home',
-      onGenerateRoute: (settings) {
-        final Widget page;
-
-        if (settings.name == RetipRoute.home.name) {
-          page = const HomePage();
-        } else if (settings.name == RetipRoute.settings.name) {
-          page = const SettingsPage();
-        } else {
-          throw Exception('Unknown route: ${settings.name}');
-        }
-
-        return MaterialPageRoute(
-          builder: (context) => page,
-          settings: settings,
-        );
-      },
+      home: const IntroPage(),
     );
   }
 }
